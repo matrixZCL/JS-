8639e615d75d16945f5d323a106510c6b7f65764
zcl <1835078104@qq.com>
2019-10-25 11:59:10 +0800

    删除食物

--- js/Food.js
+++ js/Food.js
@@ -1,3 +1,9 @@
+var position='absolute';
+//记录上次创建的食物，为删除做准备
+var elements=[];
+
+
+
 function Food(options){
     options=options || {};
     this.x=options.x||0;
@@ -7,20 +13,34 @@ function Food(options){
     this.color=options.color||'green';
 }
 
-var position='absolute';
 Food.prototype.render=function(map){
+    //删除之前创建的食物
+    remove();
     //随机生成x,y
     this.x=Tools.getRandom(0,map.offsetWidth/this.width-1)*this.width;
     this.y=Tools.getRandom(0,map.offsetHeight/this.height-1)*this.height;
     //动态创建食物div
     var div=document.createElement('div');
     map.appendChild(div);
+    elements.push(div);
+
     div.style.position=position;
     div.style.left=this.x+'px';
     div.style.top=this.y+'px';
     div.style.width=this.width+'px';
     div.style.height=this.height+'px';
     div.style.backgroundColor=this.color;
+}
+
+function remove(){
+    for(var i=elements.length-1;i>=0;i--){
+        //删除div
+        elements[i].parentNode.removeChild(elements[i]);
+
+        //删除数组中的元素
+        //第一个参数：从哪个元素开始，第二个参数：删除几个元素
+        elements.splice(i,1);
+    }
 }
 
 //test
